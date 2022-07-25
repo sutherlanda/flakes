8184a44e8aa545ecd7c47450c49d95ed3a87e7f2
Andrew Sutherland <sutherlanda@gmail.com>
2022-07-24 20:08:54 -0600

    Fix keybindings

--- neovim/config/lua/global.lua
+++ neovim/config/lua/global.lua
@@ -83,5 +83,5 @@ vim.api.nvim_set_keymap('n', '<leader>b', '<cmd>Telescope buffers<CR>', opts)
 
 -- nvim-tree
 require('nvim-tree').setup()
-vim.api.nvim_set_keymap('n', '<leader>t', '<cmd>NvimTreeToggle<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>tt', '<cmd>NvimTreeToggle<CR>', opts)
 vim.api.nvim_set_keymap('n', '<leader>tf', '<cmd>NvimTreeFocus<CR>', opts)
