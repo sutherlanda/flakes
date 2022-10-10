68f8244738b69ae48ee373fe7f8e0b8155b4d35d
Andrew Sutherland <sutherlanda@gmail.com>
2022-10-09 19:38:37 -0600

    make fn global

--- neovim/config/lua/lsp.lua
+++ neovim/config/lua/lsp.lua
@@ -1,10 +1,10 @@
+local function buf_set_keymap(...)
+  vim.api.nvim_buf_set_keymap(bufnr, ...)
+end
+
 -- Executed when language server has been attached.
 local opts = { noremap = true, silent = true }
 local on_attach = function(client, bufnr)
-
-  local function buf_set_keymap(...)
-    vim.api.nvim_buf_set_keymap(bufnr, ...)
-  end
 
   local function buf_set_option(...)
     vim.api.nvim_buf_set_option(bufnr, ...)
