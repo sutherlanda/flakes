47027e4672f6beecef8e3a5b5d06bcd6e3aa0479
Andrew Sutherland <sutherlanda@gmail.com>
2022-10-31 21:37:31 -0600

    .

--- neovim/config/lua/lsp.lua
+++ neovim/config/lua/lsp.lua
@@ -188,8 +188,8 @@ nvim_lsp.gopls.setup({
 
 nvim_lsp.tsserver.setup({
   on_attach = function(client, bufnr)
-    client.resolved_capabilities.document_formatting = false
-    client.resolved_capabilities.document_range_formatting = false
+    client.server_capabilities.document_formatting = false
+    client.server_capabilities.document_range_formatting = false
     on_attach(client, bufnr)
   end,
   capabilities = capabilities,
